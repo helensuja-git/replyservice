07bc977a49515848d0424acb03b9d43f15b9db83
helensuja-git <sujahelen@outlook.com>
2022-02-14 03:48:11 +0530

    Update RestServiceApplicationTest.java

--- src/test/java/com/beta/replyservice/RestServiceApplicationTest.java
+++ src/test/java/com/beta/replyservice/RestServiceApplicationTest.java
@@ -1,16 +1,39 @@
 package com.beta.replyservice;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+
 public class RestServiceApplicationTest {
+	
+	@Autowired
+	private ReplyController replyController;
+	
+	@LocalServerPort
+	private int localPort;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	
+	
 
 	@Test
 	public void contextLoads() {
 		
-		
+		assertTrue(replyController!=null);
 	}
+	
+	/*@Test
+	public void testEndpoint() throws Exception {
+		assertTrue(this.restTemplate.getForObject("https://localhost:"+localPort+"/reply/helloworld",String.class).contains("helloworld"));
+	}*/
 
 }
